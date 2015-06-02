3d37dd6811237ebdcfb14d0d52b08e502e7bbd0d
Martin Dzhonov <martin.dzhonov@gmail.com>
2015-06-02 18:37:24 +0300

    Revert 20ed94e..d7699f4
    This rolls back to commit 20ed94ed69795c3ba5dff0e80ca36dd059826384.

--- Overtreding/www/js/app.js
+++ Overtreding/www/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova']);
+var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova']).constant('ApiEndpoint', {url: 'http://localhost:8100/api'});
 
 app.run(function($ionicPlatform) {
 
