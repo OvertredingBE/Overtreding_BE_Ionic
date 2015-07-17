c48847ad5ecae64db364868079d42185a51cc1ec
Martin Dzhonov <martin.dzhonov@gmail.com>
2015-07-17 13:23:14 +0300

    strLenght filter

--- Overtreding_BE/www/js/app.js
+++ Overtreding_BE/www/js/app.js
@@ -58,3 +58,13 @@ app.run(function($ionicPlatform) {
     });
     $urlRouterProvider.otherwise("/config");
 });
+app.filter('strLimit', ['$filter', function($filter) {
+   return function(input, limit) {
+      if (! input) return;
+      if (input.length <= limit) {
+          return input;
+      }
+
+      return $filter('limitTo')(input, limit) + '...';
+   };
+}]);
